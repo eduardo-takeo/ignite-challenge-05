dabd8bd051e0e1af08a5634080ae897bec506ab4
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-05-23 22:50:02 -0300

    Fetch posts from Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,14 +25,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   // TODO
   return <h1>Home</h1>;
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query(
+    Prismic.Predicates.at('document.type', 'posts'),
+    { pageSize: 5, page: 1 }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      next_page: postsResponse.next_page,
+      results: {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date,
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      },
+    };
+  });
+
+  return {
+    props: { postsPagination: posts },
+  };
+};
